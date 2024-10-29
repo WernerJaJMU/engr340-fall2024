3a24e97f960ce91d6426fc70cb0a2c9a280000af
WernerJaJMU <wernerja@dukes.jmu.edu>
2024-10-29 12:00:50 -0400

    c

--- Module 7 - Signal Processing/3 - Practice/9.3.1 - Plotting EKG/plot_ekg_template.py
+++ Module 7 - Signal Processing/3 - Practice/9.3.1 - Plotting EKG/plot_ekg_template.py
@@ -6,13 +6,17 @@ import numpy as np
 path = '../../../data/ekg/mitdb_201.csv'
 
 # load data in matrix from CSV file; skip first two rows
-
+Data = np.loadtxt(path, skiprows= 2, delimiter= ',')
 ### Your code here ###
 
 # save each vector as own variable
-
+elapsedTime = Data[:,0]
+MLII = Data[:,1]
+V1 = Data[:,2]
 ### Your code here ###
 
 # use matplot lib to generate a single
-
+plt.plot(elapsedTime, V1)
+plt.plot(elapsedTime, MLII)
+plt.show()
 ### Your code here ###
--- Module 7 - Signal Processing/3 - Practice/9.3.2 - Processing EKG Data/ekg-signal-processing-template.py
+++ Module 7 - Signal Processing/3 - Practice/9.3.2 - Processing EKG Data/ekg-signal-processing-template.py
@@ -20,17 +20,27 @@ Step #1: load data in matrix from CSV file; skip first two rows. Call the data s
 
 signal = 0
 ## YOUR CODE HERE ##
-
+data = np.loadtxt(signal_filepath, skiprows= 2, delimiter= ',')
+elapsedTime = data[:,0]
+MLII = data[:,1]
+V1 = data[:,2]
+plt.title('Signal for ' + database_name)
+plt.plot(data)
+plt.show()
 """
 Step 2: (OPTIONAL) pass data through LOW PASS FILTER (fs=250Hz, fc=15, N=6). These may not be correctly in radians
 """
+
 
 ## YOUR CODE HERE ##
 
 """
 Step 3: Pass data through weighted differentiator
 """
-
+difference = np.diff(V1)
+plt.title('Difference Signal for ' + database_name)
+plt.plot(difference)
+plt.show()
 ## YOUR CODE HERE ##
 
 
@@ -39,12 +49,17 @@ Step 4: Square the results of the previous step
 """
  ## YOUR CODE HERE ##
 
+sq = np.square(difference)
+plt.title('Squared Signal for ' + database_name)
+plt.plot(sq)
+plt.show()
 """
 Step 5: Pass a moving filter over your data
 """
-
+compare = np.ones(7)
+signal = np.convolve(sq, compare)
 ## YOUR CODE HERE
 # make a plot of the results. Can change the plot() parameter below to show different intermediate signals
-plt.title('Process Signal for ' + database_name)
+plt.title('Filtered Signal for ' + database_name)
 plt.plot(signal)
 plt.show()
