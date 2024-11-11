bf88fe8f13b00c92a224a2b55d50d9ad223d5df2
WernerJaJMU <wernerja@dukes.jmu.edu>
2024-11-11 12:29:47 -0500

    c

--- Module 7 - Signal Processing/3 - Practice/9.3.3 - EKG Detection with Threshold/simple-ekg-detection-template.py
+++ Module 7 - Signal Processing/3 - Practice/9.3.3 - EKG Detection with Threshold/simple-ekg-detection-template.py
@@ -32,10 +32,10 @@ Adjust the values for threshold and timeout to change the detection method/appro
 """
 
 # set a detection threshold (YOUR VALUE BELOW)
-detection_threshold = -1
+detection_threshold = 1
 
 # set a heart beat time out (YOUR VALUE BELOW)
-detection_time_out = -1
+detection_time_out = 150
 
 # track the last time we found a beat
 last_detected_index = -1
@@ -53,6 +53,10 @@ Step 4: Manually iterate through the signal and apply the threshold with timeout
 # loop through signal finding beats
 for value in signal:
     ## Use a conditional statement to see if the signal is above a threshold...
+    if value > detection_threshold and (current_index - last_detected_index) > detection_time_out:
+        beats_detected.append(current_index)
+        last_detected_index = current_index
+
 
     ## Once an index is found, place the index in the beats_detected list
     current_index += 1
